dfc169956040a8015b5fd5a4e2ebb9122851810d
Bassam Mustafa <basam_wadi@yahoo.com>
2025-10-29 16:36:48 +0300

    Header Nearly

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
+import Link from "next/link";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Navbar() {
+  const navbarRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const navbar = navbarRef.current;
+
+    // Create a GSAP timeline linked to scroll position
+    gsap.to(navbar, {
+      backgroundColor: "rgba(0, 0, 0, 1)",
+      y: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: document.body,
+        start: "top top",
+        end: "200px top", // scroll distance over which animation happens
+        scrub: true, // makes it follow scroll smoothly
+      },
+    });
+  }, []);
+
+  return (
+    <header
+      ref={navbarRef}
+      className="fixed top-0 left-0 w-full z-50 flex justify-between items-center px-16 py-4 text-white"
+      style={{
+        backgroundColor: "rgba(0,0,0,0)",
+        transform: "translateY(45px)",
+      }}
+    >
+      <div className="max-w-[120px] absolute">
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="Logo" width={600} height={600} />
+        </Link>
+      </div>
+      <div className="flex gap-6 mx-auto">
+        <Link href="#about" className="text-white-700 hover:text-white">
+          About
+        </Link>
+        <Link href="#work" className="text-white-700 hover:text-white">
+          Work
+        </Link>
+        <Link href="#services" className="text-white-700 hover:text-white">
+          Services
+        </Link>
+        <Link href="#pricing" className="text-white-700 hover:text-white">
+          Pricing
+        </Link>
+        <Link href="#blog" className="text-white-700 hover:text-white">
+          Blog
+        </Link>
+      </div>
+    </header>
+  );
+}
---
+++ app/components/NavigationButton.tsx
@@ -0,0 +1,61 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { gsap } from "gsap";
+import { useRef, useEffect } from "react";
+
+export default function NavigationButton() {
+  const arrowRef = useRef<HTMLDivElement | null>(null);
+  const tl = gsap.timeline({ paused: true });
+  useEffect(() => {
+    tl.to(arrowRef.current, {
+      y: -48,
+      x: 48,
+      ease: "none",
+      duration: 0.3,
+    });
+  });
+  const handleEnter = () => {
+    tl.play();
+  };
+  const handleOut = () => {
+    tl.reverse();
+  };
+
+  return (
+    <Link
+      href="#"
+      className="flex flex-row text-[#212121]"
+      onMouseEnter={handleEnter}
+      onMouseLeave={handleOut}
+    >
+      <div className="bg-white py-[13px] px-[26px] rounded-2xl font-medium">
+        Book 1:1 Meeting
+      </div>
+
+      <div className="bg-white w-[48px] aspect-square flex items-center justify-center rounded-2xl relative overflow-hidden">
+        <div
+          ref={arrowRef}
+          className="absolute top-0 right-0 grid grid-cols-2 grid-rows-2 w-[96px] aspect-square place-items-center "
+        >
+          <div className="col-start-2 items-center justify-center w-[24px] aspect-square">
+            <Image
+              src="/images/top-right-arrow.svg"
+              alt="top right arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+          <div>
+            <Image
+              src="/images/top-right-arrow.svg"
+              alt="top right arrow"
+              width={24}
+              height={24}
+            />
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
