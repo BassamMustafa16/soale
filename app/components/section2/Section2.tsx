984e9826fb5d321db70d73242b93b2fa69d04887
Bassam Mustafa <basam_wadi@yahoo.com>
2025-11-02 16:21:46 +0200

    All section2 html is done

--- app/components/section2/Section2.tsx
+++ app/components/section2/Section2.tsx
@@ -11,6 +11,22 @@ import AccountIcon from "../svgs/AccountIcon";
 import PersonIcon from "../svgs/PersonIcon";
 
 export default function Section2() {
+  const images = [
+    { id: 1, image: "home-section2-person1.png", alt: "Person 01" },
+    { id: 2, image: "home-section2-person2.png", alt: "Person 02" },
+    { id: 3, image: "home-section2-person3.png", alt: "Person 03" },
+    { id: 4, image: "home-section2-person4.png", alt: "Person 04" },
+  ];
+
+  const Checklist = [
+    { id: 1, content: "Adjust font sizes for mobile", checked: true },
+    { id: 2, content: "Swap hero visuals or video", checked: true },
+    { id: 3, content: "Align content spacing & margins", checked: true },
+    { id: 4, content: "Replace or update images", checked: true },
+    { id: 5, content: "Optimize for SEO basics", checked: false },
+    { id: 6, content: "Tweak layout responsiveness", checked: false },
+    { id: 7, content: "Refine brand color usage", checked: false },
+  ];
   const arrowRef = useRef<HTMLDivElement | null>(null);
   const tl = gsap.timeline({ paused: true });
 
@@ -18,7 +34,7 @@ export default function Section2() {
     tl.to(arrowRef.current, {
       y: -48,
       x: 48,
-      ease: "power1.out",
+      ease: "power2.inOut",
       duration: 0.3,
     });
     return () => {
@@ -121,9 +137,85 @@ export default function Section2() {
               fonts, color codes, social templates & more.
             </span>
           </p>
+          <button className="bg-white text-black hover:bg-brand-orange hover:text-white transition-all duration-300 cursor-pointer rounded-2xl py-2">
+            Download Toolkit
+          </button>
         </div>
         {/* Card 03 */}
-        <div className=" md:col-span-2 border"></div>
+        <div className=" md:col-span-2 space-y-5 flex flex-col justify-between">
+          {/* Top Message */}
+          <div className="flex flex-row gap-5 py-2 px-4 bg-white rounded-full items-center justify-center">
+            {/* Mail Notification Icon */}
+            <div className="border-black w-14 aspect-square">
+              <Image
+                src="/images/mail-notification.svg"
+                alt="Mail Notifications"
+                width={54}
+                height={54}
+              />
+            </div>
+            <p className="grow text-[#212121] font-medium text-base">
+              Need to fine-tune something after launch?
+            </p>
+            {/* Images */}
+            <div className="flex flex-row">
+              {images.map((image) => (
+                <div
+                  key={image.id}
+                  className="-mr-4 w-14 aspect-square last:mr-0 border-white border-2 rounded-full"
+                >
+                  <Image
+                    src={`/images/${image.image}`}
+                    alt={image.alt}
+                    width={200}
+                    height={200}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Grayed Content */}
+          <div className="bg-[#141414] p-5 rounded-4xl space-y-5 flex flex-col justify-between grow">
+            {/* Checklist */}
+            <div className="grid grid-cols-2 gap-5">
+              {Checklist.map((item) => (
+                <div key={item.id} className="flex flex-row gap-3 text-sm">
+                  <div
+                    className={`w-6 h-6 ${
+                      !item.checked && "border"
+                    } border-white-500 rounded-full`}
+                  >
+                    {item.checked && (
+                      <Image
+                        src="/images/checked.svg"
+                        alt="Checked Icon"
+                        width={24}
+                        height={24}
+                      />
+                    )}
+                  </div>
+                  <p className={`${item.checked && "text-white-500"}`}>
+                    {item.content}
+                  </p>
+                </div>
+              ))}
+            </div>
+            {/* Grayed Content Footer */}
+            <div>
+              <p>Valid for 90 days after launch</p>
+              <p>
+                Includes up to 5 minor revisions{" "}
+                <span className="text-white-500">– no extra cost.</span>
+              </p>
+            </div>
+          </div>
+          {/* Card Footer */}
+          <p className="text-sm text-white-500">
+            We’ve got you cover with
+            <span className="text-white"> Subtle updates, site maintained</span>
+          </p>
+        </div>
       </div>
     </section>
   );
